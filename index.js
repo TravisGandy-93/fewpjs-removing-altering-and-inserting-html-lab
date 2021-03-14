534d45439ef4a9972f13c14f523a02ff2cef6801
travis_gandy <travis_gandy@yahoo.com>
2021-03-14 15:46:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+var el = document.getElementById('main');
+el.remove();
+
+let element = document.createElement('div');
+element.id= "victory";
+document.body.appendChild(element);
+let newDiv = document.querySelector('div#victory');
+newDiv.innerHTML = "<h1>Travis is the champion</h1>"
